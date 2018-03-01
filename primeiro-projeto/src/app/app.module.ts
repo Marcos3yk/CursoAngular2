0d80a24f28949f448ec55e55b83dfb4a0a0b53e8
marcos3yk <marcos3yk@gmail.com>
2018-02-28 21:51:15 -0400

    commit de criação do projeto

---
+++ primeiro-projeto/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { MeuPrimeiroComponenteComponent } from './meu-primeiro-componente/meu-primeiro-componente.component';
+import { CursosModule } from './cursos/cursos.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuPrimeiroComponenteComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    CursosModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
