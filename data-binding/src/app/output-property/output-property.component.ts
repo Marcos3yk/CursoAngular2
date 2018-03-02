a8c0eeedc8ca8b8c6524007a5013bda20337f0b8
marcos3yk <marcos3yk@gmail.com>
2018-03-02 15:46:08 -0400

    termino do modulo 2

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, Output, ViewChild, ElementRef } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 
 @Component({
@@ -12,15 +12,18 @@ export class OutputPropertyComponent implements OnInit {
 
   @Input() valor : number = 0;
 
+  @ViewChild('campoInput') campoValorInput : ElementRef;
+
+  
   
 
   decrementa(){
-    this.valor --;
+    this.campoValorInput.nativeElement.value --;
     this.mudouValor.emit( {novoValor: this.valor} );
   }
 
   incrementa(){
-    this.valor ++;
+    this.campoValorInput.nativeElement.value ++;
     this.mudouValor.emit( {novoValor: this.valor} );
   }
 
