6cdca780035a5b92f74e446261d5d45e59c54e0e
marcos3yk <marcos3yk@gmail.com>
2018-03-09 17:11:01 -0400

    criação do projeto Pipes

---
+++ pipes/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
+import { CamelCasePipe } from './camel-case.pipe';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExemplosPipesComponent,
+    CamelCasePipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.css']
+})
+export class ExemplosPipesComponent implements OnInit {
+
+  livro: any = {
+    titulo: 'o jardim de ossos Tess gerritsen',
+    rating: 4.54321,
+    numeroPaginas: 314,
+    preco: 44.99,
+    dataLancamento: new Date(2016, 5, 23),
+    url: 'https://www.saraiva.com.br/o-jardim-de-ossos-2637651.html'
+  };
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
