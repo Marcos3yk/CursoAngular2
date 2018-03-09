528a93596fba93bd787bfc251985f78849f1583e
marcos3yk <marcos3yk@gmail.com>
2018-03-09 11:08:39 -0400

    fim do módulo 5 - serviços

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,6 +9,7 @@ import { AppComponent } from './app.component';
 import { CursosModule } from './cursos/cursos.module';
 import { CursosService } from './cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { LogService } from './shared/log.service';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import { CriarCursoModule } from './criar-curso/criar-curso.module';
     CriarCursoModule,
     CursosModule
   ],
+  providers: [LogService],
   //providers: [CursosService],
   bootstrap: [AppComponent]
 })
