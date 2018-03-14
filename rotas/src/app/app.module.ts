f84bd94f3929cdd0110f4b12c8bd8d3fd6d7930d
marcos3yk <marcos3yk@gmail.com>
2018-03-13 20:30:34 -0400

    rotas com parametros e redirecionamento

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { routing } from './app.routing';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos/cursos.service';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 
 
 @NgModule({
@@ -14,13 +17,15 @@ import { routing } from './app.routing';
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule, 
     routing
   ],
-  providers: [],
+  providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- rotas/src/app/cursos/cursos.component.ts
+++ rotas/src/app/cursos/cursos.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CursosService } from './cursos.service';
+
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
-
-  constructor() { }
+  cursos: any[];
+  constructor(private cursosService: CursosService) { }
 
   ngOnInit() {
+    this.cursos = this.cursosService.getCursos();
   }
 
 }
